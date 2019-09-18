0a89ade84f903794241e78eaa097c8dc7a04e2a3
Jan Guzman <janfrancisco19@gmail.com>
2019-09-18 12:16:32 +0200

    Cleaning imports

--- resolvers/anime.go
+++ resolvers/anime.go
@@ -3,9 +3,9 @@ package resolvers
 import (
 	"context"
 
-	gqlgen "github.com/Krystian19/cactus-bff/gql"
+	"github.com/Krystian19/cactus-bff/gql"
 )
 
-func (r *queryResolver) Anime(ctx context.Context) (*gqlgen.Anime, error) {
-	return &gqlgen.Anime{ID: "1", Title: "Here we are "}, nil
+func (r *queryResolver) Anime(ctx context.Context) (*gql.Anime, error) {
+	return &gql.Anime{ID: "1", Title: "Here we are "}, nil
 }
