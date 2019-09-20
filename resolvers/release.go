976ddfaceb804d2ac53f51e5c3939d08547b17b6
Jan Guzman <janfrancisco19@gmail.com>
2019-09-20 11:55:46 +0200

    Working with the release resolvers

--- resolvers/release.go
+++ resolvers/release.go
@@ -1,6 +1,9 @@
 package resolvers
 
 import (
+	"context"
+
+	"github.com/Krystian19/cactus-bff/gql"
 	"github.com/Krystian19/cactus-core/proto"
 )
 
@@ -14,4 +17,50 @@ func releaseServiceClient() (client proto.ReleaseServiceClient, err error) {
 	}
 
 	return proto.NewReleaseServiceClient(conn), nil
+}
+
+func (r *queryResolver) Release(ctx context.Context, id *int) (*proto.Release, error) {
+	client, err := releaseServiceClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	request := &proto.ReleaseRequest{}
+
+	if id != nil {
+		request.Id = int64(*id)
+	}
+
+	response, err := client.Release(ctx, request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Release, nil
+}
+
+func (r *queryResolver) Releases(ctx context.Context, filter *gql.ReleasesFilter) ([]*proto.Release, error) {
+	client, err := releaseServiceClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	request := &proto.ReleasesRequest{Query: &proto.ReleaseQuery{}}
+
+	if filter != nil {
+		if filter.AnimeID != nil {
+			request.Query.AnimeId = int64(*filter.AnimeID)
+		}
+	}
+
+	response, err := client.Releases(ctx, request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Releases, nil
 }
