b0f603326239deef282018efe2f2386b8a922cdb
Jan Guzman <janfrancisco19@gmail.com>
2019-09-20 20:12:05 +0200

    Working with the genre methods

---
+++ resolvers/genre.go
@@ -0,0 +1,74 @@
+package resolvers
+
+import (
+	"context"
+
+	"github.com/Krystian19/cactus-bff/gql"
+	"github.com/Krystian19/cactus-core/proto"
+)
+
+type genreResolver struct{ *Resolver }
+
+func genreServiceClient() (client proto.GenreServiceClient, err error) {
+	conn, err := InitGRPCConnection()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return proto.NewGenreServiceClient(conn), nil
+}
+
+func (r *queryResolver) Genre(ctx context.Context, id *int) (*proto.Genre, error) {
+	client, err := genreServiceClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	request := &proto.GenreRequest{}
+
+	if id != nil {
+		request.Id = int64(*id)
+	}
+
+	response, err := client.Genre(ctx, request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Genre, nil
+}
+
+func (r *queryResolver) Genres(ctx context.Context, filter *gql.GenresFilter) (*gql.GenrePaginatedList, error) {
+	client, err := genreServiceClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	request := &proto.GenresRequest{Query: &proto.GenreQuery{}}
+
+	if filter != nil {
+		if filter.Title != nil {
+			request.Query.Title = *filter.Title
+		}
+
+		if filter.Limit != nil {
+			request.Query.Limit = int64(*filter.Limit)
+		}
+
+		if filter.Offset != nil {
+			request.Query.Offset = int64(*filter.Offset)
+		}
+	}
+
+	response, err := client.Genres(ctx, request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &gql.GenrePaginatedList{Rows: response.Genres, Count: int(response.Count)}, nil
+}
