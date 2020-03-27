72ca057e49768e15930c94356b725a7a2d38601e
Jan Guzman <janfrancisco19@gmail.com>
2020-03-27 01:19:30 +0100

    Making the main file slim

--- bin/main.go
+++ bin/main.go
@@ -1,52 +1,15 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/99designs/gqlgen/handler"
-	"github.com/Krystian19/cactus-bff/gql"
-	"github.com/Krystian19/cactus-bff/resolvers"
-)
+import "log"
 
 const (
-	defaultPort      = "3000"
-	gqlPlaygroundURL = "/playground_graphql"
+	// PORT : GraphQL Server port
+	PORT = "3000"
 )
 
 func main() {
-	PORT := os.Getenv("PORT")
-
-	if PORT == "" {
-		PORT = defaultPort
-	}
-
-	// Check for important env vars
-	EnvVarsCheck()
-
-	gqlConfig := gql.Config{Resolvers: &resolvers.Resolver{}}
-
-	http.Handle("/", handler.Playground("GraphQL playground", gqlPlaygroundURL))
-	http.Handle(gqlPlaygroundURL, handler.GraphQL(gql.NewExecutableSchema(gqlConfig)))
-
-	http.Handle("/graphql", handler.GraphQL(
-		gql.NewExecutableSchema(gqlConfig),
-
-		// Disable introspection for the endpoint exposed to the outside
-		handler.IntrospectionEnabled(false),
-
-		// Limit the query complexity of the endpoint exposed to the outside
-		// handler.ComplexityLimit(5), // GQL query complexity limit
-	))
-
-	log.Printf("GraphQL playground @ http://localhost:%s/", PORT)
-	log.Fatal(http.ListenAndServe(":"+PORT, nil))
-}
-
-// EnvVarsCheck : Checks that important ENV vars are set
-func EnvVarsCheck() {
-	if os.Getenv("CACTUS_CORE_URL") == "" {
-		panic("CACTUS_CORE_URL env var is not set")
+	log.Printf("GraphQL server running @ http://localhost:%s/", PORT)
+	if err := Server(PORT); err != nil {
+		log.Fatal(err)
 	}
 }
